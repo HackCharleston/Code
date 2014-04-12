d58e010b147593d083a584393262e2a87f285796
Will Jamieson <jamiesonwm@g.cofc.edu>
2014-04-11 20:47:18 -0400

    Random commit

--- Historic Landscapes App/src/main/java/org/lolt/historiclandscapes/app/MapActivity.java
+++ Historic Landscapes App/src/main/java/org/lolt/historiclandscapes/app/MapActivity.java
@@ -1,17 +1,22 @@
 package org.lolt.historiclandscapes.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 
 public class MapActivity extends ActionBarActivity {
+
+    ListView feed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        feed = (ListView) findViewById(R.id.listView);
     }
 
 
@@ -30,9 +35,81 @@ public class MapActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, LoginActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public class MyAdapter extends ArrayAdapter<Location> {
+        private int mResource;
+
+        public MyAdapter(Context context, int resource, List<Location> yaks) {
+            super(context, resource, yaks);
+
+            mResource = resource;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final String content = getItem(position).getLocation();
+            Log.e("Peek", content);
+            View view;
+
+            if (convertView == null) {
+                LayoutInflater layoutInflater = (LayoutInflater) getContext()
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = layoutInflater.inflate(mResource, parent, false);
+            } else {
+                view = convertView;
+            }
+
+            TextView locs = (TextView) view.findViewById(R.id.location);
+            locs.setText(content);
+
+            notifyDataSetChanged();
+
+            return view;
+
+        }
+    }
+
+    public class Location
+    {
+        private String text;
+        private String lat;
+        private String lon;
+        private String image;
+
+
+        public Location(String t, String lon, String lat, String d)
+        {
+            this.lat= lat;
+            this.lon =lon;
+            text = t;
+            image = d;
+        }
+
+        public String getImage()
+        {
+            return image;
+        }
+
+        public String getLat()
+        {
+            return lat;
+        }
+
+        public String getLon()
+        {
+            return lon;
+        }
+
+        public String getLocation()
+        {
+            return text;
+        }
+    }
+
 
 }
