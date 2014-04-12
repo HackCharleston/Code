417902bb5978c79824cc3850db40aa1c04db3414
John Zeringue <john.joseph.zeringue@gmail.com>
2014-04-12 10:36:42 -0400

    Played with landscapes more.

--- Historic Landscapes App/src/main/java/org/lolt/historiclandscapes/app/AdminInputActivity.java
+++ Historic Landscapes App/src/main/java/org/lolt/historiclandscapes/app/AdminInputActivity.java
@@ -7,29 +7,24 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
 
 import com.loopj.android.http.RequestParams;
 
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class AdminInputActivity extends ActionBarActivity {
 
@@ -37,16 +32,15 @@ public class AdminInputActivity extends ActionBarActivity {
     ImageView iv;
     Location l;
     Bitmap imageBitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_input);
         getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#b85f0c")));
 
-
         GPSTracker gps = new GPSTracker(this);
-         l = gps.getLocation();
-
+        l = gps.getLocation();
     }
 
 
@@ -62,12 +56,10 @@ public class AdminInputActivity extends ActionBarActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-             imageBitmap = (Bitmap) extras.get("data");
-             iv = (ImageView) findViewById(R.id.imageView);
+            imageBitmap = (Bitmap) extras.get("data");
+            iv = (ImageView) findViewById(R.id.imageView);
             iv.setBackground(null);
             iv.setImageBitmap(imageBitmap);
-
-
         }
     }
 
@@ -83,181 +75,55 @@ public class AdminInputActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
     public File saveMapAsFile(Bitmap bm) throws IOException {
+        Log.d("saveMapAsFile", "Saving map to file...");
+
         String path = Environment.getExternalStorageDirectory().toString();
         OutputStream fOut = null;
-        File file = new File(path, "images"+".jpg");
+        File file = new File(path, "landscape_image.jpg");
+
+        file.createNewFile();
+
         try {
             fOut = new FileOutputStream(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
-        bm.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+        Log.d("saveMapAsFile", "Compressing to JPEG...");
+
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
         fOut.flush();
         fOut.close();
+
+        Log.d("saveMapAsFile", "File saved as " + file.getAbsolutePath() + ".");
 
         return file;
 
     }
 
-
-    public void takePhoto()
-    {
+    public void takePhoto(View view) {
         dispatchTakePictureIntent();
     }
 
     public void submit(View view) throws IOException {
+        view = (RelativeLayout) view.getParent();
+
         //Send stuff to db here
-        String lat = l.getLatitude()+"";
-        String lon = l.getLongitude()+"";
+        String lat = l.getLatitude() + "";
+        String lon = l.getLongitude() + "";
         RequestParams params = new RequestParams();
-        params.add("file", saveMapAsFile(imageBitmap).getAbsolutePath());
+
+        params.put("image", saveMapAsFile(imageBitmap));
         params.add("latitude", lat);
         params.add("longitude", lon);
 
-        Client.post("URLHERE", params);
+        String name = ((EditText) view.findViewById(R.id.name)).getText() + "";
+        params.add("name", name);
 
+        String description = ((EditText) view.findViewById(R.id.description)).getText() + "";
+        params.add("description", description);
 
-
+        Client.post("landscapes", params);
     }
-
-/*
-
-    public int uploadFile(String sourceFileUri) {
-
-
-        String fileName = sourceFileUri;
-
-        HttpURLConnection conn = null;
-        DataOutputStream dos = null;
-        String lineEnd = "\r\n";
-        String twoHyphens = "--";
-        String boundary = "*****";
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1 * 1024 * 1024;
-        File sourceFile = new File(sourceFileUri);
-
-        if (!sourceFile.isFile()) {
-
-
-            Log.e("uploadFile", "Source File not exist :"+imagepath);
-
-            runOnUiThread(new Runnable() {
-                public void run() {
-                    //messageText.setText("Source File not exist :"+ imagepath);
-                }
-            });
-
-            return 0;
-
-        }
-        else
-        {
-            try {
-
-                // open a URL connection to the Servlet
-                FileInputStream fileInputStream = new FileInputStream(sourceFile);
-                URL url = new URL(upLoadServerUri);
-
-                // Open a HTTP  connection to  the URL
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setDoInput(true); // Allow Inputs
-                conn.setDoOutput(true); // Allow Outputs
-                conn.setUseCaches(false); // Don't use a Cached Copy
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Connection", "Keep-Alive");
-                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-                conn.setRequestProperty("uploaded_file", fileName);
-
-                dos = new DataOutputStream(conn.getOutputStream());
-
-                dos.writeBytes(twoHyphens + boundary + lineEnd);
-                dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
-                        + fileName + "\"" + lineEnd);
-
-                dos.writeBytes(lineEnd);
-
-                // create a buffer of  maximum size
-                bytesAvailable = fileInputStream.available();
-
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                buffer = new byte[bufferSize];
-
-                // read file and write it into form...
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                while (bytesRead > 0) {
-
-                    dos.write(buffer, 0, bufferSize);
-                    bytesAvailable = fileInputStream.available();
-                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                }
-
-                // send multipart form data necesssary after file data...
-                dos.writeBytes(lineEnd);
-                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-                // Responses from the server (code and message)
-                serverResponseCode = conn.getResponseCode();
-                String serverResponseMessage = conn.getResponseMessage();
-
-                Log.i("uploadFile", "HTTP Response is : "
-                        + serverResponseMessage + ": " + serverResponseCode);
-
-                if(serverResponseCode == 200){
-
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-                            String msg = "File Upload Completed.\n\n See uploaded file here : \n\n"
-                                    +" F:/wamp/wamp/www/uploads";
-                            //messageText.setText(msg);
-                            //Toast.makeText(MainActivity.this, "File Upload Complete.", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-
-                //close the streams //
-                fileInputStream.close();
-                dos.flush();
-                dos.close();
-
-            } catch (MalformedURLException ex) {
-
-                //dialog.dismiss();
-                ex.printStackTrace();
-
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        messageText.setText("MalformedURLException Exception : check script url.");
-                        Toast.makeText(MainActivity.this, "MalformedURLException", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
-            } catch (Exception e) {
-
-                dialog.dismiss();
-                e.printStackTrace();
-
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                       // messageText.setText("Got Exception : see logcat ");
-                        //Toast.makeText(MainActivity.this, "Got Exception : see logcat ", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                Log.e("Upload file to server Exception", "Exception : " + e.getMessage(), e);
-            }
-            dialog.dismiss();
-            return serverResponseCode;
-
-        } // End else block
-    }
-*/
-
 }
